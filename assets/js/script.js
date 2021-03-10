0495fa1133c1806ad8cee878f47bbf0423e72592
Karen Roh <karenroh@hotmail.com>
2021-03-10 17:00:21 -0500

    when click start button, 1st question with answers pop up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 // DOM elements
 const startButton = document.getElementById('start-btn')
+const cardEl = document.getElementById('card-questions')
+const newCardEl = document.getElementById('question-container')
+
+// const shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
@@ -8,6 +12,11 @@ startButton.addEventListener('click', startGame)
 function startGame() {
     console.log('Started')
     startButton.classList.add('hide')
+    cardEl.classList.add('hide')
+    cardEl.parentNode.replaceChild(newCardEl, cardEl)
+    newCardEl.classList.remove('hide')
+    newCardEl.classList.add('my-card')
+    setNextQuestion()
 }
 
 // setting next question (what happens when click on next button)
@@ -20,21 +29,33 @@ function selectAnswer() {
 
 }
 
-var secondsLeft = 60;
-
-function startTimer() {
-    var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timeClock.textContent = secondsLeft + "";
-        if (secondsLeft === 0) {
-            clearInterval(timerInterval);
-            return("Quiz Over");
-        }
-
+const questions = [
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            { text: '4', correct: true },
+            { text: '22', correct: false }
+        ]
+    }
+]
 
 
-    }, 1000);
-}
+// for timer (use if needed)
+// var secondsLeft = 60;
+
+// function startTimer() {
+//     var timerInterval = setInterval(function() {
+//         secondsLeft--;
+//         timeClock.textContent = secondsLeft + "";
+//         if (secondsLeft === 0) {
+//             clearInterval(timerInterval);
+//             return("Quiz Over");
+//         }
+
+
+
+//     }, 1000);
+// }
 
 // steps need to take:
 // 1. when click on start, question #1 comes up

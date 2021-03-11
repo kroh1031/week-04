fe2a3319cc743be9f3b8e83dcb2474c1b9344662
Karen Roh <karenroh@hotmail.com>
2021-03-10 23:23:36 -0500

    got rid of unnecessary answer buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // DOM elements
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
 const cardEl = document.getElementById('card-questions')
 const newCardEl = document.getElementById('question-container')
 const questionEl = document.getElementById('question')
@@ -25,23 +26,41 @@ function startGame() {
 
 // setting next question (what happens when click on next button)
 function setNextQuestion() {
+  resetState()
   showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
     questionEl.innerText = question.question
-    // question.answers.forEach(answer => {
-    //     const button = document.createElement('button')
-    //     button.innerText = answer.text
-    //     button.classList.add('btn')
-    //     if (answer  )
-    // })
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsEl.appendChild(button)
+    })
 
+}
+
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsEl.firstChild) {
+        answerButtonsEl.removeChild
+        (answerButtonsEl.firstChild)
+    }
 }
 
 // do something when select answer
 function selectAnswer() {
-
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
 }
 
 const questions = [
